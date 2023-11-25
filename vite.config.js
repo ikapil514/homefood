e8e238db690417f1cb12386f7d3bef1a30713275
ikapil514 <kapilagarwal514@gmail.com>
2023-11-25 20:46:27 +0530

    g

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/homefood/",
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/homefood/",
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/homefood/";
+  }
+
+  return config;
 });
